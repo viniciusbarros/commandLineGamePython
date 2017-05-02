ab0c2daca6c6ce585018b137e808b8de8b93721b
Vinicius Barros <vmbarross@gmail.com>
2017-05-02 09:15:00 +0100

    Main Class - Menu + Game loop + Quit method

--- main.py
+++ main.py
@@ -1,28 +1,53 @@
 from char import Char
 from tinydb import TinyDB, Query
+from useful import ColourPrint
+from useful import GenericMenu
+import os
 import json
 
 
 class Game:
+    logged_in = False
+    menu_options = {
+        'login': 'Do login',
+        'quit': 'Quit game'
+    }
+    char = None
+
     def __init__(self):
         self.db = TinyDB('db/database.json')
-        # char_table = db.table('character')
-        self.welcome()
 
-    def welcome(self):
-        name = raw_input("Please, enter your name ")
+        #Game Loop
+        while True:
+            if self.char is None or self.char.logged_in is False:
+                os.system('clear')
+                GenericMenu.generic_menu("Main Menu", "What do you want to do?", self.menu_options, self)
+            else:
 
-        char = Char(name)
-        result = char.search(name)
+                GenericMenu.generic_menu("Char Menu", "What do you want to do?", self.char.char_menu, self.char)
+
+
+    def login(self):
+        os.system('clear')
+        ColourPrint.print_line("Login")
+        name = raw_input("Please, enter your name: ")
+
+        self.char = Char(name)
+        result = self.char.search(name)
 
         if len(result) == 0:
-            char.save_to_db()
-            print("Looks like it is your first time here, {}".format(name))
+            self.char.save_to_db()
+            print("Welcome {0}! Looks like it is your first time here.".format(name))
         else:
+            self.char.set(result[0])
             print("Welcome back, {}".format(name))
-            # char.set(result)
+            # char.level_up()
+            # char.save_to_db()
 
-            # char.print_status()
+        self.char.logged_in = True
 
+    def quit(self):
+        ColourPrint.print_blue("Bye Bye")
+        quit()
 
 Game()
